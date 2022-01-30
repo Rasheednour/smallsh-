112fa7f0f7dd229fdb2b799b45dc3f3c8d15343c
Rasheed Mohammed <mohamras@os1.engr.oregonstate.edu>
2022-01-30 10:36:56 -0800

    implemented function to remove trrailing and leading white space from command inputted by user

--- main.c
+++ main.c
@@ -2,10 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 void exitShell()
 {
     printf("Ending processes and exiting shell\n");
+}
+
+char *removeSpaces(char *command)
+{
+    char *commandEnd;
+
+    while (isspace((unsigned char) *command))
+    {
+        command++;
+    } 
+
+    if (*command == 0)
+    {
+        return command;
+    }
+
+    commandEnd = command + strlen(command) - 1;
+
+    while (commandEnd > command && isspace ((unsigned char) *commandEnd))
+    {
+        commandEnd--;
+    } 
+
+    commandEnd[1] = '\0';
+
+    return command;
 }
 
 void changeDirectory()
@@ -33,28 +60,32 @@ void commandPrompt()
         printf(": ");
         fgets(command, 2048, stdin);
         command[strlen(command) - 1] = 0;
-        if (strcmp(command, "exit") == 0)
+
+        // remove trailing and leading spaces 
+        char *newCommand = removeSpaces(command);
+
+        if (strcmp(newCommand, "exit") == 0)
         {
             exitShell();
             break;
         }
-        else if ((strcmp(command, "") == 0) || (command[0] == '#')) // fix multiple spaces
+        else if ((strcmp(newCommand, "") == 0) || (newCommand[0] == '#')) // fix multiple spaces
         {
             continue;
         }
-        else if (strcmp(command, "pwd") == 0)
+        else if (strcmp(newCommand, "pwd") == 0)
         {
             char path[255];
             getcwd(path, sizeof(path));
             printf("Current working directory is %s\n", path);
         }
-        else if (strcmp(command, "cd") == 0)
+        else if (strcmp(newCommand, "cd") == 0)
         {
             changeDirectory();
         }
         else
         {
-        printf("You entered: %s\n", command);
+        printf("You entered: %s\n", newCommand);
         }
         
     }
