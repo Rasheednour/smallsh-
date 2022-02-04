391288eefca848cb5578b98dc09e4e62e51a78ae
Rasheed Mohammed <mohamras@os1.engr.oregonstate.edu>
2022-02-04 08:27:57 -0800

    implemented several structs to store background processes and status codes

--- main.c
+++ main.c
@@ -13,8 +13,29 @@
 // FIX CD WHEN ONE OF THE FILES IN PATH DOES NOT EXIST
 // UPDATE white space stripper to remove extra spaces between command and arguments
 // IMPLEMENT A TOKENIZER TO RETURN A LIST OF COMMAND AND ITS ARGUMENTS along with & if exists
+
+struct backgroundPID
+{
+    int pid;
+    struct backgroundPID *next;
+};
+
+struct statusCode
+{
+    char *message;
+    int code;
+};
+
+struct parsedCommand
+{
+    char *command;
+    char **args;
+    char *count;
+};
+
 void exitShell()
 {
+    // take an array of pids for background processes and kill them
     printf("Ending processes and exiting shell\n");
 }
 
@@ -59,41 +80,70 @@ void printPath()
     printf("The path is: %s\n", path);
 }
 
-int executeCommand(char *command)
+void executeVP(char** commandList)
+{
+    // execute parsed list of command and arguments
+}
+
+void executeLP(char *command)
+{
+    // execute a single command
+}
+
+void storeBgProcess(struct backgroundPID *bgList)
+{
+    // add new background PID to the tail of linked lists of background processes
+}
+
+void executeCommand(char *command, int execMode, struct statusCode *exitStatus, struct backgroundPID *bgList)
 {
     // printf("Command is: %s\n", command);
     printf("parent PID is: %d\n", getpid());
-    printf("forking child..\n");
+    printf("backgorund process number: %d\n", bgList->pid);
+    // printf("forking child..\n");
 
-    pid_t spawnPid = -5;
-    int childExitStatus = -5;
+    // pid_t spawnPid = -5;
+    // int childExitStatus = -5;
 
-    spawnPid = fork();
+    // spawnPid = fork();
 
-    switch (spawnPid) {
-        case -1: { perror("Fork unsuccessful!\n"); exit(1); break; }
-        case 0: {
-            printf("New child forked with pid %d and parnet's pid is %d\n", getpid(), getppid());
-            sleep(1);
-            printf("Child with pid %d now executing command: %s\n", getpid(), command);
-            sleep(2);
-            execlp(command, command, NULL);
-            perror("child exec failure!\n");
-            exit(2); break;
-        }
-        default: {
-            printf("parent with pid %d now sleeping for 2 secs\n", getpid());
-            sleep(2);
-            printf("parent with pid %d now waiting for child with pid %d to terminate\n", getpid(), spawnPid);
-            pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
-            printf("parent with pid %d: child with pid %d terminated, now exiting!\n", getpid(), actualPid);
-            exit(0); break;
-        }
-    }
+    // switch (spawnPid) {
+    //     case -1: { perror("Fork unsuccessful!\n"); exit(1); break; }
+    //     case 0: {
+    //         printf("New child forked with pid %d and parnet's pid is %d\n", getpid(), getppid());
+    //         sleep(1);
+    //         printf("Child with pid %d now executing command: %s\n", getpid(), command);
+    //         sleep(2);
+    //         execlp(command, command, NULL);
+    //         perror("child exec failure!\n");
+    //         exit(2); break;
+    //     }
+    //     default: {
+    //         printf("parent with pid %d now sleeping for 2 secs\n", getpid());
+    //         sleep(2);
+    //         printf("parent with pid %d now waiting for child with pid %d to terminate\n", getpid(), spawnPid);
+    //         pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
+    //         printf("parent with pid %d: child with pid %d terminated, now exiting!\n", getpid(), actualPid);
+    //         exit(0); break;
+    //     }
+    // }
 
-    fork();
+
+
+    // get main command
+    // 
 
     
+}
+
+void openInputFile()
+{
+
+}
+
+void openOutputFile()
+{
+
 }
 
 void changeDirectory(char *path) // dont change directories if any of them is invalid
@@ -122,46 +172,86 @@ void changeDirectory(char *path) // dont change directories if any of them is in
     }
 }
 
-void updateStatus(int statusCode)
-{
-    printf("exit value %d\n", statusCode);
-}
 
-void commandTokenizer()
+void tokenizer(char *command)
 {
 
 }
 
 void commandPrompt()
 {
-    int statusCode = 0;
-    updateStatus(statusCode);
+
+//----------------------Initiate struct to track execution status--------------
+
+    // initiate statusCode struct and set its message to "exit value 0"
+    // this statusCode struct will be later passed to other functions when executing commands using exec()
+    // and its value will be updated regulary with each terminated process so that it always holds the 
+    // exit status for the last terminated process.
+    struct statusCode *exitStatus = malloc(sizeof(struct statusCode));
+    char *msg = "exit value";
+    int value = 0;
+    exitStatus->message = msg;
+    exitStatus->code = value;
+
+//----------------------Initiate a struct to store background PIDs--------
+
+    struct backgroundPID *bgList = malloc(sizeof(struct backgroundPID));
+    bgList->pid = -1;
+    bgList->next = NULL;
+
+//----------------------Initiate a variable to store execution mode--------
+
+    // "1" is for background mode, and "0" is for foreground mode.
+    int execMode = 0;
+
+//----------------------------Enter command prompt loop------------------------
 
     while(1)
     {
+
+    //---------------------show prompt and get user input---------------------
+
+        // background processes must be checked here before user is handed control
         char command[2048];
         printf(": ");
+        // get user input
         fgets(command, 2048, stdin);
+        // remove new line at the end of user input
         command[strlen(command) - 1] = 0;
 
-        // remove trailing and leading spaces 
+    //--------------remove trailing and leading spaces from command-----------
+
+        // call function to remove the leading and trailing empty space -if exit- in the user entered command
+        // store the new command in a new variable
         char *newCommand = removeSpaces(command);
 
+    //--------------------------------Set execution mode--------------------------------------------
+        //*****check for empty inputs
+        // check if last letters in command is " &"
+        int len = strlen(newCommand);
+        if (len > 1)
+        {
+            const char *lastTwo = &newCommand[len - 2];
+            if (strcmp(lastTwo, " &") == 0)
+            {
+                execMode = 1;
+            }
+            else
+            {
+                execMode = 0;
+            }
+        }
+    //----------------------------------------------------------------------------------------------
         if (strncmp(newCommand, "exit", 4) == 0)
         {
             exitShell();
             break;
         }
-        else if ((strcmp(newCommand, "") == 0) || (newCommand[0] == '#')) // fix multiple spaces
+        else if ((strcmp(newCommand, "") == 0) || (newCommand[0] == '#')) 
         {
             continue;
         }
-        else if (strcmp(newCommand, "pwd") == 0)
-        {
-            char path[255];
-            getcwd(path, sizeof(path));
-            printf("Current working directory is %s\n", path);
-        }
+
         else if (strcmp(newCommand, "cd") == 0)
         {
             homeDirectory();
@@ -172,26 +262,28 @@ void commandPrompt()
         }
         else if (strncmp(newCommand, "status", 6) == 0)
         {
-            updateStatus(0);
+            printf("%s %d\n", exitStatus->message, exitStatus->code);
         }
-        else if (strcmp(newCommand, "pid") == 0)
-        {
-            printf("My pid is %d\n", getpid());
-        }
-        else if (strcmp(newCommand, "ppid") == 0)
-        {
-            printf("My parent's pid is %d\n", getppid());
-        }
-        else if (strcmp(newCommand, "path") == 0)
-        {
-            printPath();
-        }
+
         else
         {
-            executeCommand(newCommand);
+            executeCommand(newCommand, execMode, exitStatus, bgList);
         }
         
     }
+    
+    // user is exiting
+    // kill background processes inside of exit function while passing it PID list
+    // free status code and background PID list
+    free(exitStatus);
+    struct backgroundPID *tmp;
+    while (bgList != NULL)
+    {
+        tmp = bgList;
+        bgList = bgList->next;
+        free(tmp);
+    }
+    
 
 }
 
